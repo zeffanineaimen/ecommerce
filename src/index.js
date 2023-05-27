daa60fb67c450afb1b7a3ebe520e2106a22a4639
zeffanine aimen <zeffanineaimen21@gmail.com>
2023-05-27 10:37:00 +0100

    most sold product

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all.min.js'
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item));
+
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert("Ietm was added to the card")
+    });
+});
